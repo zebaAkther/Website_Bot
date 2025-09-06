c98fed93a1581e78d2606448f3030c8d4c6b1990
zebaAkther <zebaakther94@gmail.com>
2025-09-06 11:57:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   const [question, setQuestion] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
   const [loading, setLoading] = useState(false);
-  const API_BASE_URL = "https://hosted-app-llm-bot-atmn.onrender.com"; // Replace with your backend URL
+  const API_BASE_URL = " https://website-bot-6.onrender.com"; // Replace with your backend URL
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
